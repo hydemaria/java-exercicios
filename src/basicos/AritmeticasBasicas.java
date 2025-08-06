60d05313287378a934f412216e74acc1e08c6dc3
Maria Luiza Lima <paganimarialuiza.s@gmail.com>
2025-08-05 22:43:23 -0300

    feat: implementa operações básicas (soma, subtração, multiplicação, divisão) e adiciona nota sobre resto da divisão

--- src/basicos/AritmeticasBasicas.java
+++ src/basicos/AritmeticasBasicas.java
@@ -10,7 +10,38 @@ Saída esperada :
         125 dividido por 24 = 5
         */
 
-        package basicos;
+package basicos;
+
+import java.util.Scanner;
 
 public class AritmeticasBasicas {
+        public static void main(String[] args) {
+                Scanner caixa = new Scanner(System.in);
+                System.out.printf("Digite o primeiro numero: ");
+                int primeiro = caixa.nextInt();
+
+                System.out.println("Digite o segundo numero: ");
+                int segundo = caixa.nextInt();
+
+                int soma = primeiro + segundo;
+                int sub = primeiro - segundo;
+                int produto = primeiro * segundo;
+                int divisao = primeiro / segundo;
+                // int resto = primeiro % segundo; n declarei esse carinha aqui
+
+                System.out.println(primeiro + " + " + segundo + " = " + soma);
+                System.out.println(primeiro + " - " + segundo + " = " + sub);
+                System.out.println(primeiro + " x " + segundo + " = " + produto);
+                System.out.println(primeiro + " / " + segundo + " = " + divisao);
+                System.out.printf(primeiro + " dividido por " + segundo + " = " + divisao);
+
+                /* Nota de aprendizado:
+                n me atentei ao resto da divisao, n colocarei no meu codigo pois realmente n fiz
+                mas a titulo de aprendizado e consultas futuras eh assim q se faz o resto:
+                resto = primeiro % segundo;
+                System.out.println(primeiro + " mod " + segundo + " = " + resto); // <--- imprime o resto
+                 */
+
+        caixa.close();
+        }
 }
